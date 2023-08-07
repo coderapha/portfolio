215b49ffda30621981c7da70fc079a424a316ecc
coderapha <raphaelcardoso@email.com>
2023-08-06 21:57:32 -0300

    Altera a identidade visual

--- src/App-redirect.jsx
+++
@@ -1,5 +0,0 @@
-function Redirect() {
-  window.location.href = "https://www.youtube.com/watch?v=G7KNmW9a75Y";
-}
-
-export default Redirect;
--- src/main-redirect.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Redirect from "./App-redirect";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Redirect />
-  </React.StrictMode>
-);
